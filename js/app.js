569a751c172448425cefa06d9e77da7b1c139517
SapnaDorbi <savydorbi@gmail.com>
2020-09-05 22:15:35 +0530

    worked on getBagButtons functionality

--- js/app.js
+++ js/app.js
@@ -48,15 +48,43 @@ class UI {
         });
         productsDOM.innerHTML = result;
     }
+
+    getBagButtons() {
+        const buttons = [...document.querySelectorAll('.bag-btn')];
+        // console.log(buttons, "check buttons");
+        buttons.forEach(button => {
+            let id = button.dataset.id;
+            // console.log(id, "check id");
+            let inCart = cart.find(item => item.id === id);
+            if(inCart) {
+                button.innerHTML = "In Cart";
+                button.disabled = true;
+            } else {
+                button.addEventListener('click', (event) => {
+                    event.target.innerHTML = "In Cart";
+                    event.target.disabled = true;
+                });
+            }
+        });
+    }
 }
 
 //local storage
-class Storage {}
+class Storage {
+
+    static saveProducts(products) {
+        localStorage.setItem("products", JSON.stringify(products));
+    }
+
+}
 
 document.addEventListener("DOMContentLoaded",() => {
     const ui = new UI();
     const products = new Products();
 
     //get all products
-    products.getProducts().then(products => ui.displayProducts(products));
+    products.getProducts().then(products => {
+        ui.displayProducts(products);
+        Storage.saveProducts(products);
+    }).then(() => ui.getBagButtons());
 });
